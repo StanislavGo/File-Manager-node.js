ab56a5fe20c7a1e32f3663175108be2bb1b9051d
“Stanislav <gorokht6mail.ru@gmail.com>
2024-02-01 23:23:08 +0200

    feat: create function to greet the user

---
+++ src/modules/greetingFunc.js
@@ -0,0 +1,8 @@
+const greetingFunction = () => {
+  const cliArgs = process.argv.slice(2);
+
+  const usernameArg = cliArgs.find((el) => el.startsWith("--username="));
+  const username = usernameArg.split("=")[1];
+
+  console.log(`Welcome to the File Manager, ${username || 'Username'}!`);
+}
